1f746d97c6851b50835924f781ef24cf8a2cd4a6
Bishal Tamang <bishal.tamang.dev@gmail.com>
2024-10-26 10:35:45 +0545

    type conversion: to integer

---
+++ type_conversion/to_integer.dart
@@ -0,0 +1,17 @@
+void main() {
+  int intVar;
+
+  // boolean :: dart doesn't provide direct method
+  bool boolVar = true;
+  intVar = boolVar ? 1 : 0;
+  print("Bool: $intVar");
+
+  // double
+  double doubleVar = 123.456;
+  print("\nRemoving decimal part: ${doubleVar.toInt()}");
+  print("Rounding to the nearest integer: ${doubleVar.round()}");
+  print("Round up to the next integer: ${doubleVar.ceil()}");
+  print("Round down to the previous integer: ${doubleVar.floor()}");
+  print(
+      "Convert directly to an integer string: ${int.parse(doubleVar.toStringAsFixed(0))}");
+}
